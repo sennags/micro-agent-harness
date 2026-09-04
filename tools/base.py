3062fb8959de28016572a573d41c4731f45c3e23
sennags <sillasgs@outlook.com>
2026-09-04 19:44:55 -0300

    gitignore

--- tools/base.py
+++ tools/base.py
@@ -1,1 +1,17 @@
-#Implementação do ToolRegistry
+#Descrição de uma função para a LLM puxar
+
+{
+    "type": "function",
+    "function": {
+        "name": "somar",
+        "description": "Somar dois inteiros.",
+        "parameters": {
+            "type": "objetc",
+            "properties": {
+                "a": { "type": "integer" },
+                "b": { "type": "integer" }
+            },
+            "required": ["a", "b"]
+        }
+    }
+}
